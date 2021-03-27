a235d1c245cf19d4dd42bb900f497dc4321b24d3
D4n13l3k00 <D4n13l3k00@yandex.ru>
2021-03-27 22:47:35 +0300

    Add module

---
+++ callcontrol.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Module author: @dekftgmodules
+
+from .. import loader
+from telethon import functions
+from asyncio import sleep
+@loader.tds
+class VGCallControllerMod(loader.Module):
+    "Control group voice calls"
+    strings = {"name": "VGCallController"}
+    @loader.owner
+    async def callstartcmd(self, m):
+        "Start call in chat"
+        if not m.chat: return await m.edit("<b>[VGCallController]</b> It is not a chat!")
+        call = (await m.client(functions.channels.GetFullChannelRequest(m.chat.id))).full_chat.call
+        if not call:
+            try:
+                await m.client(functions.phone.CreateGroupCallRequest(peer=m.chat))
+                await m.edit("<b>[VGCallController]</b> Call started!")
+                await sleep(5)
+            except: await m.edit("<b>[VGCallController]</b> Err...")
+        else:
+            await m.edit("<b>[VGCallController]</b> There is call now!")
+    async def callstopcmd(self, m):
+        "Stop call in chat"
+        if not m.chat: return await m.edit("<b>[VGCallController]</b> It is not a chat!")
+        call = (await m.client(functions.channels.GetFullChannelRequest(m.chat.id))).full_chat.call
+        if call:
+            try:
+                await m.client(functions.phone.DiscardGroupCallRequest(call))
+                await m.edit("<b>[VGCallController]</b> Call stopped!")
+                await sleep(5)
+            except: await m.edit("<b>[VGCallController]</b> Err...")
+        else:
+            await m.edit("<b>[VGCallController]</b> There is no call now!")
