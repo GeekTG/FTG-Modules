a51ee534a7045e797eda48461a86cf3e20936687
D4n13l3k00 <D4n13l3k00@yandex.ru>
2021-04-03 17:05:17 +0300

    updating modules

--- callcontrol.py
+++ callcontrol.py
@@ -2,7 +2,7 @@
 
 # Module author: @dekftgmodules
 
-from .. import loader
+from .. import loader, utils
 from telethon import functions
 from asyncio import sleep
 
@@ -15,28 +15,27 @@ class VGCallControllerMod(loader.Module):
     @loader.owner
     async def callstartcmd(self, m):
         "Start call in chat"
-        if not m.chat: return await m.edit("<b>[VGCallController]</b> It is not a chat!")
+        if not m.chat: return await utils.answer(m, "<b>[VGCallController]</b> It is not a chat!")
         call = (await m.client(functions.channels.GetFullChannelRequest(m.chat.id))).full_chat.call
         if not call:
             try:
                 await m.client(functions.phone.CreateGroupCallRequest(peer=m.chat))
-                await m.edit("<b>[VGCallController]</b> Call started!")
-                await sleep(5)
+                await utils.answer(m, "<b>[VGCallController]</b> Call started!")
             except:
-                await m.edit("<b>[VGCallController]</b> Err...")
+                await utils.answer(m, "<b>[VGCallController]</b> Err...")
         else:
-            await m.edit("<b>[VGCallController]</b> There is call now!")
+            await utils.answer(m, "<b>[VGCallController]</b> There is call now!")
 
     async def callstopcmd(self, m):
         "Stop call in chat"
-        if not m.chat: return await m.edit("<b>[VGCallController]</b> It is not a chat!")
+        if not m.chat: return await utils.answer(m, "<b>[VGCallController]</b> It is not a chat!")
         call = (await m.client(functions.channels.GetFullChannelRequest(m.chat.id))).full_chat.call
         if call:
             try:
                 await m.client(functions.phone.DiscardGroupCallRequest(call))
-                await m.edit("<b>[VGCallController]</b> Call stopped!")
+                await utils.answer(m, "<b>[VGCallController]</b> Call stopped!")
                 await sleep(5)
             except:
-                await m.edit("<b>[VGCallController]</b> Err...")
+                await utils.answer(m, "<b>[VGCallController]</b> Err...")
         else:
-            await m.edit("<b>[VGCallController]</b> There is no call now!")
+            await utils.answer(m, "<b>[VGCallController]</b> There is no call now!")
