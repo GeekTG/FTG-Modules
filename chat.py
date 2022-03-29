a4ae4c2b87f446a33faccaee0b663634a27f4350
KOT4N <dimasofienko464@gmail.com>
2022-03-29 18:56:48 +0300

    remove echo

--- chat.py
+++ chat.py
@@ -414,30 +414,3 @@ class ChatMod(loader.Module):
         await message.edit("<b>Ты получил репорт за спам!</b>")
         await sleep(1)
         await message.delete()
-
-    async def echocmd(self, message):
-        """Активировать/деактивировать Echo."""
-        echos = self.db.get("Echo", "chats", [])
-        chatid = str(message.chat_id)
-
-        if chatid not in echos:
-            echos.append(chatid)
-            self.db.set("Echo", "chats", echos)
-            return await message.edit("<b>[Echo Mode]</b> Активирован в этом чате!")
-
-        echos.remove(chatid)
-        self.db.set("Echo", "chats", echos)
-        return await message.edit("<b>[Echo Mode]</b> Деактивирован в этом чате!")
-
-    async def watcher(self, message):
-        echos = self.db.get("Echo", "chats", [])
-        chatid = str(message.chat_id)
-
-        if chatid not in str(echos):
-            return
-        if message.sender_id == (await message.client.get_me()).id:
-            return
-
-        await message.client.send_message(
-            int(chatid), message, reply_to=await message.get_reply_message() or message
-        )
